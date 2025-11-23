dc58461858dd6ce2a12cbce49db3fc1942724ed0
plugsPakkawat <plugs2004@gmail.com>
2025-11-23 04:39:47 +0000

    define header structure

---
+++ src/proc_interface.h
@@ -0,0 +1,7 @@
+#ifndef PROC_INTERFACE_H
+#define PROC_INTERFACE_H
+
+int  create_exec_log_proc(void);
+void remove_exec_log_proc(void);
+
+#endif /* PROC_INTERFACE_H */
---
+++ src/trace_handler.h
@@ -0,0 +1,7 @@
+#ifndef TRACE_HANDLER_H
+#define TRACE_HANDLER_H
+
+int  register_exec_trace_handler(void);
+void unregister_exec_trace_handler(void);
+
+#endif /* TRACE_HANDLER_H */
